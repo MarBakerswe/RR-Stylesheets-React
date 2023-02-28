31525acd911b770d952ef736a99f91ecee3d623c
Marlon Baker <marbaker.swe@gmail.com>
2023-02-27 22:51:17 -0800

    Final commit

---
+++ src/Sidebar.js
@@ -0,0 +1,17 @@
+const styledText = {
+    fontSize: "25px",
+    textAlign: "center",
+    margin: "40px",
+    border: "1px solid green",
+    color: "green",
+    backgroundColor: "yellow"
+  };
+  
+  export default function Example() {
+    return (
+      <div style={styledText}>
+        <p>Green Text With Yellow Background</p>
+      </div>
+    );
+    }
+  
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Example from './Example';
+import Sidebar from './Sidebar';
+import Posts from './Posts';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Example />
+    <Sidebar />
+    <Posts />
   </React.StrictMode>,
   document.getElementById('root')
 );
